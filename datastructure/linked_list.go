fc7ce3528e0659917d99d3a479d8cf6625620461
Jett <zhengjiajie@mudu.tv>
2022-01-27 19:06:22 +0800

    add folder

--- datastructure/linked_list.go
+++ datastructure/linked_list.go
@@ -124,6 +124,25 @@ func mergeTwoSortedListsRecursively(l1, l2 *ListNode) *ListNode {
 	return l2
 }
 
+func mergeTwoSortedListsIteratively(l1, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		r := mergeTwoSortedListsRecursively(l1.Next, l2)
+		l1.Next = r
+		return l1
+	}
+
+	r := mergeTwoSortedListsRecursively(l1, l2.Next)
+	l2.Next = r
+	return l2
+}
+
 // 23 https://leetcode.com/problems/merge-k-sorted-lists/
 func mergeKLists(lists []*ListNode) *ListNode {
 	if len(lists) == 0 {
